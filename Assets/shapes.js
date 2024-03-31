e39154edd0a333024895084b64058139cac1d0f4
Nick Sprague <n.sprague18@gmail.com>
2024-03-30 22:31:19 -0400

    used git init to create package.json, created main.js where will deploy from, added require inquirer to main,js, created shapes.js which will be used to create shapes

---
+++ Assets/shapes.js
@@ -0,0 +1,6 @@
+const createShapes {
+    constructor(text, color){
+        this.text = text
+        this.color = color
+    }
+}
---
+++ main.js
@@ -0,0 +1,3 @@
+const inquirer = require('inquirer');
+const {square, circle, triangle} = require('./Assets/shape');
+const createShape = inquirer.createPromptMpdule();
